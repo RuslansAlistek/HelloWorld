126da7db53c5a78f8444220dabfeddffa0c69b9b
Ruslan <ruslvasiljevs@yandex.com>
2015-08-09 15:53:19 +0300

    Added time output

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,10 @@ Created on 09 ���. 2015 �.
 @author: ������
 '''
 
+import time
+
 if __name__ == '__main__':
     phrase = "Hello World!"
     print phrase
+    
+    print time.strftime("%Y-%m-%d %H:%M:%S")

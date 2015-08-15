0f1447e88e968d11199823328c46fcef8ae75712
Ruslan <ruslvasiljevs@yandex.com>
2015-08-16 00:53:28 +0300

    IP range generator

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,25 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2014 TRESCloud S.A. (http://www.trescloud.com). All Rights Reserved
+#    
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
 '''
 Created on 09 ���. 2015 �.
 
@@ -12,3 +33,4 @@ if __name__ == '__main__':
     print phrase
     
     print "Date & Time", time.strftime("%Y-%m-%d %H:%M:%S") # Current date and time
+    
---
+++ ip.py
@@ -0,0 +1,67 @@
+# -*- encoding: utf-8 -*-
+'''
+Created on 16 авг. 2015 г.
+
+@author: Руслан
+'''
+
+#!/usr/bin/python
+
+import struct, socket
+
+RANGE_LIMIT = 80
+IP_DEC_LENGTH = 10
+IPV4_BITS = 32
+
+ip2dec = lambda ip: struct.unpack("!L", socket.inet_aton(ip))[0]
+dec2ip = lambda dec: socket.inet_ntoa(struct.pack('!L',dec))
+
+makeIdIp = lambda net_id, ip_decimal: int(str(net_id) + "%%0%sd" % IP_DEC_LENGTH % ip_decimal)
+splitIdIp = lambda Id: (int(str(Id)[:-IP_DEC_LENGTH]), int(str(Id)[-IP_DEC_LENGTH:]))
+
+if __name__ == '__main__':
+    _host_table = []
+    _host_used_by_network = {}
+
+    net1 = {'id':1, 'mask':24, 'ip':'192.168.0.0'}
+    net2 = {'id':2, 'mask':24, 'ip':'192.168.1.0'}
+    net3 = {'id':3, 'mask':29, 'ip':'186.25.10.1'}
+
+    all_ids = [
+        23232235778,23232235790,23232235791,23232235792,23232235854,23232235853,23232235856,
+        33122203138L,33122203142L,33122203141L
+    ]
+
+    for a in all_ids:
+        net_id, ip_dec = splitIdIp(a)
+        _host_used_by_network.setdefault(net_id, []).append(ip_dec)
+
+    ### CURRENT NETWORK ###
+    net = net2
+    #######################
+
+    ### return 'host_range' for function _get_hosts ###
+    shift_mask = IPV4_BITS-net['mask']
+    host_min = (ip2dec(net['ip']) >> shift_mask << shift_mask)+1
+    host_max = (ip2dec(net['ip']) >> shift_mask << shift_mask)+pow(2, shift_mask)-1
+    if host_max-host_min > RANGE_LIMIT:
+        host_max = host_min + RANGE_LIMIT + len(_host_used_by_network.get(net['id'],[]))
+    host_range = range(host_min, host_max)
+    host_range = list(set(host_range)-set(_host_used_by_network.get(net['id'],[])))
+    ###################################################
+
+    ### return 'ids' for function search ###
+    _host_table = map(lambda ip:{'id':ip,'ip':dec2ip(ip),'network_id':net['id']}, host_range)
+    ids = map(lambda ip: makeIdIp(net['id'], ip), host_range)
+    ########################################
+
+    print map(dec2ip, host_range)
+    print _host_table
+    print ids, len(ids)
+
+
+    #### return 'res' for function read ###
+    res = map(lambda id: {'id':id, 'ip':dec2ip(splitIdIp(id)[1])}, ids)
+    print res
+    #######################################
+    
